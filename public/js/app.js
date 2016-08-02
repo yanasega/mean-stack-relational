025b56c3fa3c51f953373c1906d2a77d6302e0c2
yanasega <yanasegal90@gmail.com>
2016-08-02 17:54:58 +0300

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.auth']);
+
+angular.module('mean.system', []);
+angular.module('mean.articles', []);
+angular.module('mean.auth', []);
---
+++ public/js/config.js
@@ -0,0 +1,57 @@
+//Setting up route
+angular.module('mean').config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
+
+    $urlRouterProvider.otherwise(function($injector, $location){
+        $injector.invoke(['$state', function($state) {
+            $state.go('404');
+        }]);
+    });
+    $stateProvider
+        .state('home',{
+            url : '/',
+            controller : 'IndexController',
+            templateUrl: 'views/index.html'
+        })
+        .state('SignIn',{
+            url : '/signin',
+            templateUrl: 'views/users/signin.html'
+        })
+        .state('OpenReg',{
+            url : '/openregistration',
+            templateUrl: 'views/OpenRegistration.html'
+        })
+        .state('SignUp',{
+            url : '/signup',
+            templateUrl: 'views/users/signup.html'
+        })
+        .state('articles',{
+            url : '/articles',
+            controller : 'ArticlesController',
+            templateUrl: 'views/articles/list.html'
+        })
+        .state('createArticle',{
+            url : '/articles/create',
+            controller : 'ArticlesController',
+            templateUrl: 'views/articles/create.html'
+        })
+        .state('editArticles',{
+            url : '/articles/{articleId}/edit',
+            controller : 'ArticlesController',
+            templateUrl: 'views/articles/edit.html'
+        })
+        .state('viewArticle',{
+            url : '/articles/{articleId}',
+            controller : 'ArticlesController',
+            templateUrl: 'views/articles/view.html'
+        })
+        .state('404',{
+            templateUrl: 'views/404.html'
+        })
+}
+]);
+
+//Setting HTML5 Location Mode
+angular.module('mean').config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+
+}]);
