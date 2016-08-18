51398e136b856a113a5faeeeb15fbec227dcd3c3
Alisa Khaykin <Alisa Khaykin>
2016-08-18 20:32:29 +0300

    almost finished degcalc

--- public/js/controllers/users/DegreeCalculatorController.js
+++ public/js/controllers/users/DegreeCalculatorController.js
@@ -4,32 +4,46 @@ angular.module('mean.system').controller('DegreeCalculatorController', ['$scope'
     $scope.showcourse = true;
     $scope.status = null;
     
-    // $scope.openRegistration = function() {
-    //     var course = new Courses({
-    //         coursename: $scope.coursename,
-    //         coursenumber: $scope.coursenumber,
-    //         creditpoints: $scope.creditpoints,
-    //         grade: $scope.grade
-    //     });
+     $scope.addCourse = function() {
+        var reg = new Course({
+            course: $scope.Course,
+            coursenumber: $scope.coursenumber,
+            creditpoints: $scope.creditpoints,
+            grade: $scope.grade
+           
+        });
+consule.log("alisssssssssssssssssssssssssssssssssssssssssssssssssssssssssa");
+        reg.$save(function(response) {
+            //yana: add check if response valid?
+consule.log("yanaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        });
+        $scope.status = "Course added successfully.";
 
-    // };
+    };
  
-    //  $scope.find = function() {
-    //     Courses.query(function(Course) {
-    //         $scope.Course = Course; 
-    //         $scope.showreg = true;
-    //     });
-    // };
+//      $scope.find = function() {
+//         Registrations.query(function(registrations) {
+//             $scope.registrations = registrations; //yana: check if data relavent?
+//             $scope.showreg = true;
+//         });
+//     };
 
-    // $scope.remove = function(Course) {
-    //     if (course) {
-    //         course.$remove();  
-    //     }
-    //     else {
-    //         $scope.course.$remove();
-    //     }
-    // };
+//     $scope.remove = function(registration) {
+//         if (registration) {
+//             registration.$remove();  
+
+//             // for (var i in $scope.registrations) {
+//             //     if ($scope.registrations[i] === registration) {
+//             //         $scope.registrations.splice(i, 1);
+//             //     }
+//             // }
+//         }
+//         else {
+//             $scope.registration.$remove();
+//             //$state.go('articles');
+//         }
+//     };
   
-    // $scope.find();
+//     $scope.find();
 
-}]);
+// }]);
