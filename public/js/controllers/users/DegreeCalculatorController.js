1eb89dc0a9d5527f9aa35beb63948baa4e68517d
Alisa Khaykin <Alisa Khaykin>
2016-08-18 19:32:59 +0300

    started work on DEGCAL

--- public/js/controllers/users/DegreeCalculatorController.js
+++ public/js/controllers/users/DegreeCalculatorController.js
@@ -1,4 +1,35 @@
-angular.module('mean.system').controller('DegreeCalculatorController', ['$scope', '$resource' ,'Global', '$window',function ($scope, $resource , Registrations,Global,$window) {
-    console.log("RegistrationController");
-    $scope.alisa = false;
+angular.module('mean.system').controller('DegreeCalculatorController', ['$scope', '$resource' ,'Courses','Global', '$window',function ($scope, $resource ,Courses,Global,$window) {
+    console.log("DegreeCalculatorController");
+    $scope.global = Global;
+    $scope.showcourse = true;
+    $scope.status = null;
+    
+    // $scope.openRegistration = function() {
+    //     var course = new Courses({
+    //         coursename: $scope.coursename,
+    //         coursenumber: $scope.coursenumber,
+    //         creditpoints: $scope.creditpoints,
+    //         grade: $scope.grade
+    //     });
+
+    // };
+ 
+    //  $scope.find = function() {
+    //     Courses.query(function(Course) {
+    //         $scope.Course = Course; 
+    //         $scope.showreg = true;
+    //     });
+    // };
+
+    // $scope.remove = function(Course) {
+    //     if (course) {
+    //         course.$remove();  
+    //     }
+    //     else {
+    //         $scope.course.$remove();
+    //     }
+    // };
+  
+    // $scope.find();
+
 }]);
