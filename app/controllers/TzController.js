d670af45665ec75dde038b038dd3aa5d17a27370
yanasega <yanasegal90@gmail.com>
2016-10-01 11:46:44 +0300

    working on the algorithem

--- app/controllers/TzController.js
+++ app/controllers/TzController.js
@@ -5,25 +5,39 @@
  */
 var StandardError = require('standard-error');
 var db = require('../../config/sequelize');
-var exec = require('child_process').execFile;
+
+var exec = require('child_process').exec;
+var fs = require('fs');
+var PythonShell = require('python-shell');
+var escapeJSON = require('escape-json-node');
 
 /**
- * Create a Instructor
+ * Create a tz
  */
 exports.create = function(req, res) {
     // augment the article by adding the UserId
     //req.body.UserId = req.user.id;
-    // save and return and instance of article on the res object. 
 
-    console.log("algo startttttttttttttttttttttttttttttttttttttttttt");
-    
-    exec('algoritm.py',['3','winter'], function (err, data) {                
-        console.log(data);                  
+    var options = {
+        mode: 'text',
+        pythonPath: 'python.exe',
+        // pythonOptions: ['-u'],
+        scriptPath: '..//algo',
+        args: ['3', 'winter']
+    };
+
+    PythonShell.run('algoritm.py', options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        // results = results.toString().replace(/\\/g, "");
+        JSONString = escapeJSON(results);
+        console.log(JSONString);
+
     });
 
-    console.log("algo startttttttttttttttttttttttttttttttttttttttttt");
+
+
     
-    console.log(req.body);
     db.Tz.create(req.body).then(function(tz){
         if(!tz){
             return res.send('users/signup', {errors: new StandardError('Tz could not be created')}); //yana:change the landing page.
