26e7387b7f63d27ec8e3c17df65408c5864aca27
Alisa Khaykin <Alisa Khaykin>
2016-10-05 18:54:03 +0300

    clock works

--- public/js/controllers/admin/StudentInCourseController.js
+++ public/js/controllers/admin/StudentInCourseController.js
@@ -45,6 +45,7 @@ angular.module('mean.system').controller('StudentInCourseController', ['$scope',
  
                 sic.$save(function(response) {
                      $scope.status = "Selection saved successfully.";
+                     
                 });
             }
             else{
@@ -64,6 +65,7 @@ angular.module('mean.system').controller('StudentInCourseController', ['$scope',
         // $scope.Name = null;
         // $scope.CreditPoints = null;
         $scope.status = "Selection saved successfully.";
+        console.log("$scope.status");
     }
 
    
