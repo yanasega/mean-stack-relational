2124f497b66fd0c730c6777308d67fbc628d41b4
yanasega <yanasegal90@gmail.com>
2016-10-28 11:25:06 +0300

    commit befor merge with shiran

--- app/controllers/StudentController.js
+++ app/controllers/StudentController.js
@@ -8,10 +8,11 @@ var db = require('../../config/sequelize');
 
 exports.student = function(req, res, next, id) {
     console.log('id => ' + id);
-    db.User.find({where: {id: id}}).then(function(student){
+    db.Student.find({where: {id: id}}).then(function(student){
         if(!student) {
             return next(new Error('Failed to load student ' + id));
         } else {
+            
             req.student = student;
             return next();            
         }
@@ -20,8 +21,26 @@ exports.student = function(req, res, next, id) {
     });
 };
 
+exports.create = function(req, res) {
+    // augment the article by adding the UserId
+    //req.body.UserId = req.user.id;
+    // save and return and instance of article on the res object. 
+    db.Student.create(req.body).then(function(student){
+        if(!student){
+            return res.send('users/signup', {errors: new StandardError('Student could not be created')}); //yana:change the landing page.
+        } else {
+            return res.jsonp(student);
+        }
+    }).catch(function(err){
+        return res.send('users/signup', { 
+            errors: err,
+            status: 500
+        });
+    });
+};
+
 exports.all = function(req, res) {
-    db.User.findAll().then(function(student){
+    db.Student.findAll().then(function(student){
         return res.jsonp(student);
     }).catch(function(err){
         return res.render('error', {
@@ -33,8 +52,13 @@ exports.all = function(req, res) {
 
 exports.update = function(req, res) {
     // create a new variable to hold the article that was placed on the req object.
+    
     var student = req.student;
     student.updateAttributes({
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        Email: req.body.Email,
+        Gender: req.body.Gender,
         Semester: req.body.Semester,
         CurrentYear: req.body.CurrentYear,
         Generalaverage: req.body.Generalaverage,
