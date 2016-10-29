58957801bb789d666d3b4e13961f181a505e99b5
yanasega <yanasegal90@gmail.com>
2016-10-29 17:35:46 +0300

    finished admin-user separation. started home page

--- app/routes/registrations.js
+++ app/routes/registrations.js
@@ -17,7 +17,6 @@ app.route('/registrations/:registrationId')
     .put(users.requiresLogin, registrations.update);
     // .put(users.requiresLogin, articles.hasAuthorization, articles.update)
 
-
 // Note: the registrations.registration function will be called everytime then it will call the next function.
 app.param('registrationId', registrations.registration);
 };
